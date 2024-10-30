be1d304f2c2eb939d5bec6056d2d67b0797a0147
ducvc2812 <ducvc@kimei.vn>
2024-10-31 00:01:41 +0700

    first commit

---
+++ js/google-map.js
@@ -0,0 +1,122 @@
+var google;
+
+function init() {
+    // Coordinates for Van Lang University
+    var myLatlng = new google.maps.LatLng(10.762622, 106.660172); 
+    var mapOptions = {
+        // Zoom level
+        zoom: 15, // Adjusted for a closer view
+
+        // Center the map at the university's location
+        center: myLatlng,
+
+        // Map styling options
+        scrollwheel: false,
+        styles: [
+            { elementType: 'geometry', stylers: [{ color: '#242f3e' }] },
+            { elementType: 'labels.text.stroke', stylers: [{ color: '#242f3e' }] },
+            { elementType: 'labels.text.fill', stylers: [{ color: '#746855' }] },
+            {
+                featureType: 'administrative.locality',
+                elementType: 'labels.text.fill',
+                stylers: [{ color: '#d59563' }]
+            },
+            {
+                featureType: 'poi',
+                elementType: 'labels.text.fill',
+                stylers: [{ color: '#d59563' }]
+            },
+            {
+                featureType: 'poi.park',
+                elementType: 'geometry',
+                stylers: [{ color: '#263c3f' }]
+            },
+            {
+                featureType: 'poi.park',
+                elementType: 'labels.text.fill',
+                stylers: [{ color: '#6b9a76' }]
+            },
+            {
+                featureType: 'road',
+                elementType: 'geometry',
+                stylers: [{ color: '#38414e' }]
+            },
+            {
+                featureType: 'road',
+                elementType: 'geometry.stroke',
+                stylers: [{ color: '#212a37' }]
+            },
+            {
+                featureType: 'road',
+                elementType: 'labels.text.fill',
+                stylers: [{ color: '#9ca5b3' }]
+            },
+            {
+                featureType: 'road.highway',
+                elementType: 'geometry',
+                stylers: [{ color: '#746855' }]
+            },
+            {
+                featureType: 'road.highway',
+                elementType: 'geometry.stroke',
+                stylers: [{ color: '#1f2835' }]
+            },
+            {
+                featureType: 'road.highway',
+                elementType: 'labels.text.fill',
+                stylers: [{ color: '#f3d19c' }]
+            },
+            {
+                featureType: 'transit',
+                elementType: 'geometry',
+                stylers: [{ color: '#2f3948' }]
+            },
+            {
+                featureType: 'transit.station',
+                elementType: 'labels.text.fill',
+                stylers: [{ color: '#d59563' }]
+            },
+            {
+                featureType: 'water',
+                elementType: 'geometry',
+                stylers: [{ color: '#17263c' }]
+            },
+            {
+                featureType: 'water',
+                elementType: 'labels.text.fill',
+                stylers: [{ color: '#515c6d' }]
+            },
+            {
+                featureType: 'water',
+                elementType: 'labels.text.stroke',
+                stylers: [{ color: '#17263c' }]
+            }
+        ]
+    };
+
+    // Get the HTML DOM element that will contain your map 
+    var mapElement = document.getElementById('map');
+
+    // Create the Google Map using the element and options defined above
+    var map = new google.maps.Map(mapElement, mapOptions);
+    
+    var addresses = ['Van Lang University, Ho Chi Minh City, Vietnam'];
+    const YOUR_API_KEY = '';
+
+    for (var x = 0; x < addresses.length; x++) {
+        $.getJSON('https://maps.googleapis.com/maps/api/geocode/json?address=' + encodeURIComponent(addresses[x]) + '&sensor=false&key=' + YOUR_API_KEY, null, function (data) {
+            if (data.results.length > 0) {
+                var p = data.results[0].geometry.location;
+                var latlng = new google.maps.LatLng(p.lat, p.lng);
+                new google.maps.Marker({
+                    position: latlng,
+                    map: map,
+                    icon: 'images/loc.png' // Path to your custom marker icon
+                });
+            }
+        });
+    }
+}
+
+// Load the map when the window loads
+google.maps.event.addDomListener(window, 'load', init);
